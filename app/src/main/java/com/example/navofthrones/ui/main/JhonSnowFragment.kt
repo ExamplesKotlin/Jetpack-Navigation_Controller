f5da5ac3cd07f551e41994ff4ae1fa1bb5a5b061
Henry AT <yiperu@gmail.com>
2019-09-20 20:17:42 -0300

    add fragment, both view and class

---
+++ app/src/main/java/com/example/navofthrones/ui/main/JhonSnowFragment.kt
@@ -0,0 +1,30 @@
+package com.example.navofthrones.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.navofthrones.R
+
+class JhonSnowFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = JhonSnowFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+//        return super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.jhon_snow_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // TODO: add arguments
+    }
+}
